f067522a0139871b9c46598f0198c568e78d6674
Theophilus Quachie Asenso <theophqa@Theophiluss-MacBook-Air.local>
2025-02-08 00:35:20 +0100

    minor fix

--- R/myPackage-package.R
+++ R/myPackage-package.R
@@ -2,9 +2,10 @@
 #' @import lgarch
 #' @import mgcv
 #' @import MCMCpack
-#' @import spatstat
 #' @import Matrix
 #' @import caret
 #' @import pgdraw
 #' @import plotly
+#' @import stats
+#' @import graphics
 NULL
